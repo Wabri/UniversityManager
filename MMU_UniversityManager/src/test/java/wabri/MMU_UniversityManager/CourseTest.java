e55f0e998327e3889809f8642d054aa52b69d62e
Wabri <gabriele.puliti@gmail.com>
2017-10-23 21:44:10 +0200

    Created Class Course

---
+++ MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/CourseTest.java
@@ -0,0 +1,22 @@
+package wabri.MMU_UniversityManager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CourseTest {
+
+	private Course course;
+	
+	@Before
+	public void init() {
+		course = new Course();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
