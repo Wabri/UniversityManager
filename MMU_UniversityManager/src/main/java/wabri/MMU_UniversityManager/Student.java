b3f381e6df424e2d428b524aee539ced79bd1462
Wabri <gabriele.puliti@gmail.com>
2017-10-23 19:03:25 +0200

    Tested getters and setters for the field surname of class Student

--- MMU_UniversityManager/src/main/java/wabri/MMU_UniversityManager/Student.java
+++ MMU_UniversityManager/src/main/java/wabri/MMU_UniversityManager/Student.java
@@ -3,9 +3,11 @@ package wabri.MMU_UniversityManager;
 public class Student {
 
 	private String name;
+	private String surname;
 
-	public Student(String name) {
+	public Student(String name, String surname) {
 		this.setName(name);
+		this.setSurname(surname);
 	}
 
 	public String getName() {
@@ -14,6 +16,14 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
 
 }
--- MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/StudentTest.java
+++ MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/StudentTest.java
@@ -6,20 +6,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class StudentTest {
-	
+
 	Student student;
 
 	@Before
 	public void init() {
-		student = new Student("Name");
 	}
-	
+
 	@Test
 	public void testNewStudentHaveAName() {
 		String name = "Test Name";
-		student = new Student(name);
-		
+		student = new Student(name, "Surname");
+
 		assertEquals(name, student.getName());
+	}
+
+	@Test
+	public void testNewStudentHaveASurname() {
+		String surname = "Test Surname";
+		student = new Student("Name", surname);
+		
+		assertEquals(surname, student.getSurname());
 	}
 
 }
