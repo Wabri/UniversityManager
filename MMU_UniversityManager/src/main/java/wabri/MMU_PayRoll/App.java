c816ff17d4c35995edf9518ecdba0d421a755ff2
Wabri <gabriele.puliti@gmail.com>
2017-10-23 13:07:22 +0200

    Setted Maven project

---
+++ MMU_UniversityManager/src/main/java/wabri/MMU_PayRoll/App.java
@@ -0,0 +1,13 @@
+package wabri.MMU_PayRoll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
