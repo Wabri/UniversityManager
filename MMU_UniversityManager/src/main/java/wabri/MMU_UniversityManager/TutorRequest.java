187f01e688307bcf7a0d5c43aad5da6b60cf093d
Wabri <gabriele.puliti@gmail.com>
2017-10-24 12:36:00 +0200

    Added method to get idStudent in class TutorRequest

--- MMU_UniversityManager/src/main/java/wabri/MMU_UniversityManager/TutorRequest.java
+++ MMU_UniversityManager/src/main/java/wabri/MMU_UniversityManager/TutorRequest.java
@@ -2,4 +2,32 @@ package wabri.MMU_UniversityManager;
 
 public class TutorRequest {
 
+	private Teacher teacher;
+	private Student student;
+
+	public TutorRequest(Teacher teacher, Student student) {
+		this.setTeacher(teacher);
+		this.setStudent(student);
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public String getIdStudent() {
+		return this.student.getId();
+	}
+
 }
--- MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/TutorRequestTest.java
+++ MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/TutorRequestTest.java
@@ -1,6 +1,7 @@
 package wabri.MMU_UniversityManager;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,19 +9,26 @@ import org.junit.Test;
 public class TutorRequestTest {
 
 	private TutorRequest tutorRequest;
+	private Teacher teacher;
+	private Student student;
 	
 	@Before
 	public void init() {
-		tutorRequest = createNewTestTutorRequest();
+		teacher = mock(Teacher.class);
+		student = mock(Student.class);
+		tutorRequest = createNewTestTutorRequest(teacher,student);
+		
+		when(teacher.getId()).thenReturn("idTeacher");
+		when(student.getId()).thenReturn("idStudent");
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTutorRequestHasReferenceOfStudent() {
+		assertEquals(student.getId(), tutorRequest.getIdStudent());
 	}
 	
-	private TutorRequest createNewTestTutorRequest() {
-		return new TutorRequest();
+	private TutorRequest createNewTestTutorRequest(Teacher teacher, Student student) {
+		return new TutorRequest(teacher,student);
 	}
 
 }
