f8b29e25b2bcda5af3078cf3f8dfbae0aefc86b4
Wabri <gabriele.puliti@gmail.com>
2017-10-24 12:05:12 +0200

    Creation of class CourseRequest

---
+++ MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/CourseRequestTest.java
@@ -0,0 +1,22 @@
+package wabri.MMU_UniversityManager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CourseRequestTest {
+	
+	private CourseRequest courseRequest;
+
+	@Before
+	public void init() {
+		courseRequest = new CourseRequest();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
