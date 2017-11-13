da51b2ff54193620f3d6f812d717f0ed0a54c424
Wabri <gabriele.puliti@gmail.com>
2017-11-13 19:55:25 +0100

    create test for right student finding

--- MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/SchoolControllerTest.java
+++ MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/SchoolControllerTest.java
@@ -14,7 +14,7 @@ public class SchoolControllerTest {
 	private SchoolController schoolController;
 	private Database database;
 	private List<Student> students;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		database = mock(Database.class);
@@ -29,7 +29,7 @@ public class SchoolControllerTest {
 		verify(database).getAllStudentsList();
 		assertEquals(0, allStudents.size());
 	}
-	
+
 	@Test
 	public void testGetAllStudentsWhenThereIsOneStudent() {
 		students.add(newStudentTest(""));
@@ -37,13 +37,23 @@ public class SchoolControllerTest {
 		verify(database).getAllStudentsList();
 		assertEquals(1, allStudents.size());
 	}
-	
+
 	@Test
 	public void testGetStudentByIdWhenStudentIsNotThere() {
 		students.add(newStudentTest("idTest"));
 		Student student = schoolController.getStudentByID("idWrong");
 		verify(database).findStudentById("idWrong");
 		assertNull(student);
+	}
+
+	@Test
+	public void testGetStudentByIdWhenStudentIsThere() {
+		String idStudent = "idTest";
+		students.add(newStudentTest(idStudent));		
+		when(database.findStudentById(idStudent)).thenReturn(students.get(0));
+		Student student = schoolController.getStudentByID(idStudent);
+		verify(database).findStudentById(idStudent);
+		assertNotNull(student);
 	}
 
 	private Student newStudentTest(String idStudent) {
