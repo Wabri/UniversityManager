16e34873e1ec8e0d800441b429318699ec88b7f2
Wabri <gabriele.puliti@gmail.com>
2017-11-14 19:34:05 +0100

    cant work jongo in my student class i think

--- MMU_UniversityManager/src/main/java/wabri/MMU_UniversityManager/MongoDatabaseWrapper.java
+++ MMU_UniversityManager/src/main/java/wabri/MMU_UniversityManager/MongoDatabaseWrapper.java
@@ -5,41 +5,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import com.mongodb.BasicDBObject;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 public class MongoDatabaseWrapper implements Database {
 
-	private DBCollection students;
+	private MongoCollection students;
 
 	public MongoDatabaseWrapper(MongoClient client) throws UnknownHostException {
 		super();
 		DB db = client.getDB("school");
-		students = db.getCollection("student");
+		Jongo jongo = new Jongo(db);
+		students = jongo.getCollection("student");
 	}
 
 	@Override
 	public List<Student> getAllStudentsList() {
-		DBCursor cursor = students.find();
-		return StreamSupport.stream(cursor.spliterator(), false)
-				.map(e -> new Student((String) e.get("name"), (String) e.get("surname"), (String) e.get("id"),
-						(MailService) e.get("mailService"), (UniversityDB) e.get("universityDB")))
-				.collect(Collectors.toList());
+		Iterable<Student> iterable = students.find().as(Student.class);
+		return StreamSupport.stream(iterable.spliterator(), false).collect(Collectors.toList());
 	}
 
 	@Override
 	public Student findStudentById(String idStudent) {
-		BasicDBObject searchQuery = new BasicDBObject();
-		searchQuery.put("id", idStudent);
-		DBObject findOne = students.findOne(searchQuery);
-		return findOne != null
-				? new Student((String) findOne.get("name"), (String) findOne.get("surname"), (String) findOne.get("id"),
-						(MailService) findOne.get("mailService"), (UniversityDB) findOne.get("universityDB"))
-				: null;
+		return students.findOne("{id: #}", idStudent).as(Student.class);
 	}
 
 }
--- MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/MongoDatabaseWrapperTest.java
+++ MMU_UniversityManager/src/test/java/wabri/MMU_UniversityManager/MongoDatabaseWrapperTest.java
@@ -7,31 +7,27 @@ import java.net.UnknownHostException;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
 import com.github.fakemongo.Fongo;
-import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 public class MongoDatabaseWrapperTest {
 
-	// SUT
 	private MongoDatabaseWrapper mongoDatabase;
-	// to add elements in the students table for testing
-	private DBCollection students;
+	private MongoCollection students;
 
 	@Before
 	public void setUp() throws UnknownHostException {
-		// in-memory java implementation of MongoDB
-		// so that we don't need to install MongoDB in our computer
 		Fongo fongo = new Fongo("mongo server 1");
 		MongoClient mongoClient = fongo.getMongo();
-		// make sure to drop the students table for testing
 		DB db = mongoClient.getDB("school");
 		db.getCollection("student").drop();
 		mongoDatabase = new MongoDatabaseWrapper(mongoClient);
-		students = db.getCollection("student");
+		Jongo jongo = new Jongo(db);
+		students = jongo.getCollection("student");
 	}
 
 	@Test
@@ -41,35 +37,28 @@ public class MongoDatabaseWrapperTest {
 
 	@Test
 	public void testGetAllStudentsNotEmpty() {
-		addStudentToCollection("1", "first");
-		addStudentToCollection("2", "second");
-		
+		students.insert("{id: '1',name: 'first'}");
+		students.insert("{id: '1',name: 'first'}");
+
 		assertEquals(2, mongoDatabase.getAllStudentsList().size());
 	}
-	
+
 	@Test
 	public void testFindStudentByIdNotFound() {
-		addStudentToCollection("1", "first");
-		
+		students.insert("{id: '1',name: 'first'}");
+
 		assertNull(mongoDatabase.findStudentById("2"));
 	}
-	
+
 	@Test
 	public void testFindStudentByIdFound() {
-		addStudentToCollection("1", "first");
-		addStudentToCollection("2", "second");
-		
+		students.insert("{id: '1',name: 'first'}");
+		students.insert("{id: '2',name: 'second'}");
+
 		Student findStudentById = mongoDatabase.findStudentById("2");
 		assertNotNull(findStudentById);
 		assertEquals("2", findStudentById.getId());
 		assertEquals("second", findStudentById.getName());
-	}
-
-	private void addStudentToCollection(String id, String name) {
-		BasicDBObject document = new BasicDBObject();
-		document.put("id", id);
-		document.put("name", name);
-		students.insert(document);
 	}
 
 }
